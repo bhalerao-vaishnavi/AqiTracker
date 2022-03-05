12f194f8b5c5c57a4a801ac663688c2b270ed6b8
Mayuresh123456 <kalkarmayuresh@gmail.com>
2022-03-05 13:16:20 +0530

    NEW

--- Main.py
+++ Main.py
@@ -8,6 +8,13 @@ root.title("AQI Tracker")
 root.geometry("700x700")
 root.configure(bg='light blue') 
 
+img = PhotoImage(file="landscape.png")
+label = Label(
+    root,
+    image=img
+)
+label.place(x=0, y=0)
+
 def nextPage():
     root.destroy()
     import Aqi
@@ -17,7 +24,8 @@ def prevPage():
     import WeatherApp_withicons
 
 
-button1 = tk.Button(root,text='AQI',fg='blue',font=('times new roman',18),width=8, command=nextPage)
+button1 = tk.Button(root,text='AQI',fg='blue',font=('times new roman',18),width=
+8, command=nextPage)
 button1.place(x = 150 , y= 200 , width=200 , height=60)
 
 
